05ad7896ea04ac52a7cf45b9310dd8689187364a
KaidoKasuk <kaido.kasuk@voco.ee>
2025-12-10 11:42:27 +0200

    bro code tutorial

---
+++ vite-project/src/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>my website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>home</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
