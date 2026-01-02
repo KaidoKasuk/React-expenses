6ac18e611d9927268e5a929e6490a518aa531385
KaidoKasuk <kaido.kasuk@voco.ee>
2026-01-02 11:51:55 +0200

    node modules pooled kaustad puudu :D

--- vite-project/src/Student.jsx
+++ vite-project/src/Student.jsx
@@ -1,9 +1,13 @@
+import PropTypes from "prop-types";
+
 function Student(props) {
   return (
-    <div>
+    <div className="student">
       <p>name: {props.name}</p>
+      <p>age: {props.age}</p>
+      <p>Student: {props.isStudent ? "yes" : "no"}</p>
     </div>
   );
 }
-
+Student.protoTypes = {};
 export default Student;
