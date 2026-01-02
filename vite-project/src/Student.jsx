fe0e5b785b2351c002ad9d5a1d784f204b6bba45
KaidoKasuk <kaido.kasuk@voco.ee>
2026-01-02 12:30:08 +0200

    props valis, default props vananenud

--- vite-project/src/Student.jsx
+++ vite-project/src/Student.jsx
@@ -1,13 +1,37 @@
 import PropTypes from "prop-types";
 
-function Student(props) {
+// function Student(props) {
+//   return (
+//     <div className="student">
+//       <p>name: {props.name}</p>
+//       <p>age: {props.age}</p>
+//       <p>Student: {props.isStudent ? "yes" : "no"}</p>
+//     </div>
+//   );
+// }
+
+//et konsool sulle arusaadava veateate saadaks ja endal lihtsam mis type prop on
+Student.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool,
+};
+
+//enam ei toota??????????
+Student.defaultProps = {
+  name: "Guest",
+  age: 0,
+  isStudent: false,
+};
+
+//chati lahendus opetajalt kusib
+function Student({ name = "Guest", age = 0, isStudent = false }) {
   return (
     <div className="student">
-      <p>name: {props.name}</p>
-      <p>age: {props.age}</p>
-      <p>Student: {props.isStudent ? "yes" : "no"}</p>
+      <p>name: {name}</p>
+      <p>age: {age}</p>
+      <p>Student: {isStudent ? "yes" : "no"}</p>
     </div>
   );
 }
-Student.protoTypes = {};
 export default Student;
