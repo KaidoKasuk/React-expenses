7baf89e4f2a8b2e4b9401960e4c1ebb4a0435380
KaidoKasuk <kaido.kasuk@voco.ee>
2026-01-07 11:35:06 +0200

    ül3 ExpenseItem ümber kujundamine olemasoleva komponendi abil

--- vite-project/src/ExpenseItem.jsx
+++ vite-project/src/ExpenseItem.jsx
@@ -1,18 +1,18 @@
 // //imports
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-
+import Card from "./Card";
 function ExpenseItem(props) {
   return (
     //üks item
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.data.date} />
 
       <div className="expense-item_description">
         <h2>{props.data.title}</h2>
         <div className="expense-item_price">{props.data.price}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
