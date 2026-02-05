12a1bee68fab0afd5114d1d008b42f2496c1dbe2
kasuk <student@Administrators-Mac-mini-2.local>
2026-02-05 16:08:57 +0200

    17docs: fragments, portals, useRef hooki kasutamine

---
+++ vite-project/src/UI/Error.jsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import Card from "./Card.jsx";
+import Button from "./Button";
+import "./Error.css";
+
+const BackDrop = () => {
+  return <div className="backdrop"></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className="modal">
+      <header className="header">
+        <h2>{props.title}</h2>
+      </header>
+      <div className="content">
+        <p>{props.message}</p>
+      </div>
+      <footer className="footer">
+        <Button onClick={props.onConfirm}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const Error = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={props.onConfirm}></BackDrop>,
+        document.getElementById("backdrop-root"),
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        ></ModalOverlay>,
+        document.getElementById("overlay-root"),
+      )}
+    </>
+  );
+};
+
+export default Error;
--- vite-project/src/components/NewExpense/ExpenseForm.jsx
+++ vite-project/src/components/NewExpense/ExpenseForm.jsx
@@ -1,34 +1,48 @@
-import { useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import "./ExpenseForm.css";
-import PropTypes, { func } from "prop-types";
-
+import PropTypes from "prop-types";
+import Error from "../../UI/Error.jsx";
 function ExpenseForm(props) {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredPrice, setEnteredPrice] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [error, setError] = useState(null);
+  console.log(error);
 
-  const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
-  };
-  const PriceChangeHandler = (event) => {
-    setEnteredPrice(event.target.value);
-  };
-  const DateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+  const titleInputRef = useRef();
+  const amountInputRef = useRef();
+  const dateInputRef = useRef();
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   const submitHandler = (event) => {
+    const enteredTitle = titleInputRef.current.value;
+    const enteredPrice = amountInputRef.current.value;
+    const enteredDate = dateInputRef.current.value;
+
     event.preventDefault();
+
+    if (
+      enteredTitle.trim().length == 0 ||
+      enteredPrice.trim().length == 0 ||
+      enteredDate.trim().length == 0
+    ) {
+      setError({
+        title: "Invalid Input",
+        message:
+          "Please Enter a valid title or amount or date (non empty values",
+      });
+      return;
+    }
+
     const expenseData = {
       title: enteredTitle,
       price: enteredPrice,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
-    //et tühjendada peale saatmist väljad
-    setEnteredTitle("");
-    setEnteredPrice("");
-    setEnteredDate("");
+    titleInputRef.current.value = "";
+    amountInputRef.current.value = "";
+    dateInputRef.current.value = "";
   };
   const FormEditHandler = (event) => {
     let state;
@@ -37,44 +51,51 @@ function ExpenseForm(props) {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            onChange={titleChangeHandler}
-            value={enteredTitle}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Price</label>
-          <input
-            type="number"
-            min={0.01}
-            step={0.01}
-            onChange={PriceChangeHandler}
-            value={enteredPrice}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min={2024 - 11 - 12}
-            max={2027 - 1 - 31}
-            onChange={DateChangeHandler}
-            value={enteredDate}
-          />
-        </div>
+    <>
+      {error && (
+        <Error
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></Error>
+      )}
+      <div>
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type="text" id="title" ref={titleInputRef} />
+            </div>
+            <div className="new-expense__control">
+              <label>Price</label>
+              <input
+                type="number"
+                id="price"
+                min={0.01}
+                step={0.01}
+                ref={amountInputRef}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                id="date"
+                type="date"
+                min={2024 - 11 - 12}
+                max={2027 - 1 - 31}
+                ref={dateInputRef}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="submit">Add Expense</button>
+            <button onClick={FormEditHandler} id="cancel">
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-        <button onClick={FormEditHandler} id="cancel">
-          Cancel
-        </button>
-      </div>
-    </form>
+    </>
   );
 }
 export default ExpenseForm;
